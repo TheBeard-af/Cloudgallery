2bc46bd76f3b651698cb1da65c351e58cc8f3859
TheBeard-af <afaqnadeem1993@gmail.com>
2026-02-24 17:03:37 +0400

    updated user data

--- infrastructure/user-data.sh
+++ infrastructure/user-data.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 dnf update -y
 
-# Install required packages
+# Install Node.js 18 and Git
 dnf install -y nodejs git
 
 # Install PM2 globally
 npm install -g pm2
 
-# Go to home directory
+# Switch to ec2-user
 cd /home/ec2-user
 
-# Clone GitHub repository
+# Clone repository
 git clone https://github.com/TheBeard-af/Cloudgallery.git
 
-# Navigate to backend folder
+# Navigate to backend
 cd Cloudgallery/backend
 
 # Install dependencies
 npm install
 
-# Start backend with PM2
-pm2 start server.js
+# Start app with PM2
+pm2 start server.js --name cloudgallery-backend
+
+# Save PM2 process list
 pm2 save
+
+# Configure PM2 to start on boot
+pm2 startup systemd -u ec2-user --hp /home/ec2-user
